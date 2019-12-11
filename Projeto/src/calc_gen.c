efd3bd5000c92a5c91ee094d7a6ef9649c665fb8
Surnagon <rcjunior7@latam.stefanini.com>
2019-12-10 22:02:52 -0300

    Quase funfando total

--- Projeto/src/calc_gen.c
+++ Projeto/src/calc_gen.c
@@ -4,51 +4,43 @@
 #include <stdio.h>
 #include "calc_gen.h"
 
+#define CALC_GEN_HIGHEST_NUM (10)
+
 void calc_gen_start()
 {
         // Initialization, should only be called once.
 	srand((unsigned)time(NULL));
 }
 
-unsigned calc_generate()
+unsigned calc_gen()
 {
 	unsigned select, oper_select;
 	int a, b;
 
 	oper_select = (rand() % (4)) + 1;
+         
+   a = (rand() % (CALC_GEN_HIGHEST_NUM))+1;
+   b = (rand() % (CALC_GEN_HIGHEST_NUM))+1;
 	
    switch(oper_select)
    {
       case 1:
-         a = (rand() % (11));
-         b = (rand() % (11));
          select = a+b;
          printf("\r\n %d + %d", a, b);
          printf("\r\n");
          break;
       case 2:
-         do
-         {
-         a = (rand() % (11));
-         b = (rand() % (11));
-         }while(a < b);
-         select = a-b;
-         printf("\r\n %d - %d", a, b);
+         select = abs(a-b);
+         printf("\r\n %d - %d", (a>b)?a:b, a>b?b:a);
          printf("\r\n");
          break;
       case 3:
-         a = (rand() % (11));
-         b = (rand() % (11));
          select = a*b;
          printf("\r\n %d X %d", a, b);
          printf("\r\n");
          break;
       case 4:
-         do
-         {
-         a = (rand() % (11));
-         b = (rand() % (11));
-         }while(a%b != 0);
+         a = a*b;
          select = a/b;
          printf("\r\n %d / %d", a, b);
          printf("\r\n");
@@ -56,7 +48,7 @@ unsigned calc_generate()
       default:
          printf("\r\n Houve um erro. Opcao invalida. Tente novamente");
          printf("\r\n");
-         break;
+      break;
    }
    return select;
 }
--- Projeto/src/main.c
+++ Projeto/src/main.c
@@ -1,65 +1,82 @@
 #include <stdlib.h>
+#include <string.h>
 #include "num_recog.h"
 #include "calc_gen.h"
+
+
+#define NO_ANS_TIMEOUT_MS (10000)
+#define ANS_COMPLETION_TIMEOUT (3000)
+
+
+#define voice_wrong_ans(_num,_str) \
+do\
+{ \
+   sprintf(_str,"espeak -v pt-br \"Que pena voce errou a resposta correta era %d\"",_num); \
+   system(_str); \
+}while(0)
+
+#define voice_right_ans() do{ system("espeak -v pt-br \"PARABENS. Voce acertou. \"");}while(0)
+#define voice_no_ans() do{ system("espeak -v pt-br \"Voce nao respondeu. \"");}while(0)
+
 
 int main()
 {
    unsigned result = 0, select = 0;
    bool timeout;
    char charbuffer[124];
+   char voice_str[124];
    int ret;
    unsigned * rd;
 
    T_numrecog_cotext context;
    T_numrecog_info buffer[10];
-
+   system("espeak -v pt \"configurando voz\"");
    do
    {
-      ret = numrecog_start(&context,5000,1000,buffer,10,"recogfile",
-                        charbuffer,sizeof(charbuffer));
-   if(ret)
-   {
-      printf("\r\n Erro ao iniciar");
-      return 0;
-   }
-
-   select = calc_gen();
-
-//   printf("\n operacao = %d \n", oper_select);
-
-   timeout = false;
-   do
-   {
-      rd = numrecog_read(&context,&result,&timeout);
-      if(rd!=NULL)
+      ret = numrecog_start(&context,NO_ANS_TIMEOUT_MS,ANS_COMPLETION_TIMEOUT,buffer,
+                           sizeof(charbuffer)/sizeof(charbuffer[0]),"recogfile",
+                           charbuffer,sizeof(charbuffer));
+      if(ret)
       {
-         printf("\r\n %d, certo?", result);
-         printf("\r\n");
-         if(result == select)
+         printf("\r\n Erro ao iniciar");
+         return 0;
+      }
+
+      select = calc_gen();
+
+      timeout = false;
+      do
+      {
+         rd = numrecog_read(&context,&result,&timeout);
+         if(rd!=NULL)
          {
-            printf("\r\n PARABENS!!! \n Voce acertou!");
+            printf("\r\n %d, certo?", result);
             printf("\r\n");
+            if(result == select)
+            {
+               printf("\r\n PARABENS! RESPOSTA CORRETA!!");
+               printf("\r\n");
+               voice_right_ans();
+            }
+            else
+            {
+               printf("\r\n Que pena. Voce errou. A resposta correta era %d", select);
+               printf("\r\n");
+               voice_wrong_ans(select,voice_str);
+            }
+            break;
          }
          else
          {
-            printf("\r\n Que pena. Voce errou. A resposta correta era %d", select);
-            printf("\r\n");
-         }
-         break;
-      }
-      else
-      {
-         if(timeout)
-         {
-            printf("\r\n a crianca  nao respondeu");
-         }
-         else
-         {
-//                  printf("\r\n a crianca nao respondeu ainda");
+            if(timeout)
+            {
+               printf("\r\n a crianca  nao respondeu");
+               voice_no_ans();
+            }
          }
       }
-   }
-   while(!timeout && select == 0);
+      while((!timeout) && (select));
+
    }
    while(1);
 
